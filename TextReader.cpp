8029742e27a231d397194f7735b69f434e95d386
H0lyP0tat0 <fojtikjan.02@seznam.cz>
2022-11-15 16:58:42 +0100

    Documentation

--- TextReader.cpp
+++ TextReader.cpp
@@ -4,10 +4,14 @@
 
 #include "TextReader.h"
 
+/// @brief Constructor for my TextReader class
+/// @params source is the name of the file thats being loaded
 TextReader::TextReader(string source) {
     this->source = source;
+    loadFile();
 }
 
+/// @brief loadFile is a function that scans the contents of a txt file
 void TextReader::loadFile() {
     ifstream file;
     int counter = 0;
@@ -26,6 +30,7 @@ void TextReader::loadFile() {
     split();
 }
 
+/// @brief split is a function that splits the numbers into 3 vectors, a - for the first dimension, b - for the second dimension, c - for the expected result
 void TextReader::split() {
     stringstream ss(lines);
     string s;
@@ -49,20 +54,24 @@ void TextReader::split() {
 }
 
 
+/// @brief printFile is a function that prints the contents of the vectors a, b, c. It shows the layout of the txt file
 void TextReader::printFile() {
     for (int i = 0; i < this->a.size(); i++) {
         cout << a[i] << " " << b[i] << " " << c[i] << endl;
     }
 }
 
+/// @brief getA is a getter for vector a
 vector<string> TextReader::getA() {
     return this->a;
 }
 
+/// @brief getB is a getter for vector b
 vector<string> TextReader::getB() {
     return this->b;
 }
 
+/// @brief getCis a getter for vector c
 vector<string> TextReader::getC() {
     return this->c;
 }
--- main.cpp
+++ main.cpp
@@ -4,10 +4,15 @@
 
 using namespace std;
 
+/// @brief printSolution is a function that prints the final result with the dimensions of the room int console
+/// @params a, b, c, results are numbers that will be displayed
 void printSolution(int a, int b, int c, int result) {
     cout << a << " x " << b << " - " << "expected result: " << c << " my result: " << result << endl;
 }
 
+// Fibonacci sequence https://oeis.org/A000045
+/// @brief fib is a function that returns the n-th fibonacci number
+/// @params n is the given number for the fibonacci sequence
 int fib(int n) {
     if (n <= 1) {
         return n;
@@ -15,39 +20,45 @@ int fib(int n) {
     return fib(n - 1) + fib(n - 2);
 }
 
-int tiling(const vector<string>& a, const vector<string>& b, const vector<string>& c) {
-    int a_dim, b_dim, c_dim, j, k, i;
+/// @brief tiling is a function that calculates all the possible combinations for tile placement into a room
+/// @params a, b, c are the vectors used for the calculations
+void tiling(const vector<string>& a, const vector<string>& b, const vector<string>& c) {
+    // declaring variables that I will use
+    int a_dim, b_dim, c_num, i, j, k;
     double result;
 
+    // for every record in the vectors a, b, c
     for (i = 0; i < a.size(); i++) {
+        // we get the values from the vectors
+        // a_dim and b_dim are the dimensions, c_num is the expected result
         a_dim = stoi(a[i]);
         b_dim = stoi(b[i]);
-        c_dim = stoi(c[i]);
-
+        c_num = stoi(c[i]);
         result = 1;
 
-        // If the dimension in less than 2 x 2 or if a and b are both odd the answer is always 0
+
+        // SPECIAL CASES
+        // If the dimensions are less than 2 x 2 or if a and b are both odd the answer is always 0
         if ((a_dim < 2 && b_dim < 2) || (a_dim % 2 == 1 && b_dim % 2 == 1)) {
             result = 0;
-        // If the dimension is 2 x b (a x 2) the result is reduced to the Fibonacci sequence
+        // If the dimension is 2 x b (a x 2) the result is a number from the Fibonacci sequence
         } else if ((a_dim == 2 && b_dim != 2) || (a_dim != 2 && b_dim == 2)) {
             if (a_dim == 2) {
                 result = fib(b_dim + 1);
             } else {
                 result = fib(a_dim + 1);
             }
-        // If a == b and a is even the result is reduced to this sequence https://oeis.org/A004003
+        // If a == b and a is even the result is a number from this sequence https://oeis.org/A004003
         } else if (a_dim == b_dim && a_dim % 2 == 0) {
-            // [4 * cos(j * Pi / (2 * n + 1))^2 + 4 * cos(k * Pi / (2 * n + 1))^2] where j={1..n}, k={1..n} and n=a_dim/2
-
+            // Product(j)[Product(k)[ equation ]];
+            // equation -> [4 * cos((j * Pi) / (2 * n + 1))^2 + 4 * cos((k * Pi) / (2 * n + 1))^2] where j={1..n}, k={1..n} and n=a_dim/2
             int n = a_dim / 2;
             for (j = 1; j <= n; j++) {
                 for (k = 1; k <= n; k++) {
-                    // [4*cos(j*Pi/(2*n+1))^2 + 4*cos(k*Pi/(2*n+1))^2]
                     result *= (4 * pow(cos((M_PI * j) / (2 * n + 1)), 2) + 4 * pow(cos((M_PI * k) / (2 * n + 1)), 2));
                 }
             }
-        // Else we use the function by Temperley & Fisher (1961) and Kasteleyn (1961)
+        // Else we use the function calculated by Temperley & Fisher (1961) and Kasteleyn (1961)
         // https://en.wikipedia.org/wiki/Domino_tiling#Counting_tilings_of_regions
         } else {
             for (j = 1; j <= a_dim / 2; j++) {
@@ -57,16 +68,18 @@ int tiling(const vector<string>& a, const vector<string>& b, const vector<string
             }
         }
 
-        printSolution(a_dim, b_dim, c_dim, result);
+        // printing the solution
+        printSolution(a_dim, b_dim, c_num, result);
     }
-
-    return 1;
 }
 
+/// @brief main cycle of the program
 int main() {
+    // loading the txt file
     TextReader tr("in.txt");
-    tr.loadFile();
 
+    // performing the calculations of the tiling problem
     tiling(tr.getA(), tr.getB(), tr.getC());
+
     return 0;
 }
